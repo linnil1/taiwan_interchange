4b9df9fbf8ec42b127c998335085e27b913884a3
linnil1 <linnil1.886@gmail.com>
2025-09-22 00:08:50 +0800

    remove testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,34 +12,5 @@ export default defineConfig({
 			outdir: './src/lib/paraglide',
 			disableAsyncLocalStorage: true
 		})
-	],
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					environment: 'browser',
-					browser: {
-						enabled: true,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+	]
 });
